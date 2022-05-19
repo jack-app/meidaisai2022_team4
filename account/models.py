bd627e9f8db243b6f9d0bb84cccd2581eb426faa
hirakuuuu <m.hiraku1129@gmail.com>
2022-05-19 23:19:12 +0900

    DBのモデルを作成（多分あってるはず）

---
+++ account/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+# ユーザー認証
+from django.contrib.auth.models import User
+
+# ユーザーアカウントのモデルクラス
+class Account(models.Model):
+
+    # ユーザー認証のインスタンス(1vs1関係)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # 追加フィールド
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
---
+++ timeline/models.py
@@ -0,0 +1,48 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from account.models import User
+
+# 年
+class Year(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    span = models.IntegerField(default=12)
+    start_date = models.DateTimeField()
+    published_date = models.DateTimeField(auto_now=True, null=False)
+
+    def __str__(self):
+        return self.name
+
+# 月
+class Month(models.Model):
+    year_id = models.ForeignKey(Year, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    span = models.IntegerField(default=4)
+    start_date = models.DateTimeField()
+    published_date = models.DateTimeField(auto_now=True, null=False)
+
+    def __str__(self):
+        return self.name
+
+# 週
+class Week(models.Model):
+    month_id = models.ForeignKey(Month, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    span = models.IntegerField(default=7)
+    start_date = models.DateTimeField()
+    published_date = models.DateTimeField(auto_now=True, null=False)
+
+    def __str__(self):
+        return self.name
+
+# イベント
+class Event(models.Model):
+    week_id = models.ForeignKey(Week, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    detail = models.TextField()
+    date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(auto_now=True, null=False)
+
+    def __str__(self):
+        return self.name
