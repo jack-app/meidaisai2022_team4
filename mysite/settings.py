372845f2ac619ca6aa5931700c3142fe94546b22
Lucas544875 <takayanagi691@gmail.com>
2022-05-28 23:03:59 +0900

    シークレットキーを外部ファイルに移動

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .settings_local import *
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,8 +24,6 @@ STATIC_DIR = os.path.join(BASE_DIR,"static")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!6t)9zkc9g6n62d2$b$rg&o#yah#971sf(@^3rrjdm&*89ih*y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
