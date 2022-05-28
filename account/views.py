d13d2f1e3db632f9ac4042b4bcd9aa1aae000b0b
hirakuuuu <m.hiraku1129@gmail.com>
2022-05-28 20:51:28 +0900

    ログイン機能追加

--- account/views.py
+++ account/views.py
@@ -1,9 +1,63 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView # テンプレートタグ
 from .forms import AccountForm, AddAccountForm # ユーザーアカウントフォーム
-from django.contrib.auth import authenticate
+from .models import Account # ユーザーアカウントデータベース
+
+# ログイン・ログアウト処理に利用
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+#ログイン
+def Login(request):
+    # POST
+    if request.method == 'POST':
+        # フォーム入力のユーザーID・パスワード取得
+        ID = request.POST.get('userid')
+        Pass = request.POST.get('password')
+
+        # Djangoの認証機能
+        user = authenticate(username=ID, password=Pass)
+
+        # ユーザー認証
+        if user:
+            #ユーザーアクティベート判定
+            if user.is_active:
+                # ログイン
+                login(request,user)
+                # ホームページ遷移
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                # アカウント利用不可
+                return HttpResponse("アカウントが有効ではありません")
+        # ユーザー認証失敗
+        else:
+            return HttpResponse("ログインIDまたはパスワードが間違っています")
+    # GET
+    else:
+        return render(request, 'account/login.html')
+
+
+#ログアウト
+@login_required
+def Logout(request):
+    logout(request)
+    # ログイン画面遷移
+    return HttpResponseRedirect(reverse('Login'))
+
+
+#ホーム
+@login_required
+def home(request):
+    userid = request.user
+    cur_user = Account.objects.get(user_id=userid)
+
+    params = {"UserID":request.user, "display_name":cur_user.display_name, }
+    return render(request, "account/home.html",context=params)
+
+
+
 
 class  AccountRegistration(TemplateView):
 
