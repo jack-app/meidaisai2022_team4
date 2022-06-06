f2fac2e861cbfcc3742a65b08c053b8a34fb7d87
hirakuuuu <m.hiraku1129@gmail.com>
2022-06-06 11:00:18 +0900

    シークレットキーを削除

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+try:
+    from .settings_local import *
+except ImportError:
+    pass
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # テンプレート(HTML)
@@ -23,13 +28,8 @@ STATIC_DIR = os.path.join(BASE_DIR,"static")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!6t)9zkc9g6n62d2$b$rg&o#yah#971sf(@^3rrjdm&*89ih*y'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1']
+#DEBUG -> settings_local.py
+#ALLOWED_HOSTS -> settings_local.py
 
 
 # Application definition
