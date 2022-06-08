ab4d308a29873fd155f46a894bd0e522ce7c820e
Lucas544875 <takayanagi691@gmail.com>
2022-06-08 16:46:01 +0900

    余計な設定を消した

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,10 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-try:
-    from .settings_local import *
-except ImportError:
-    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
